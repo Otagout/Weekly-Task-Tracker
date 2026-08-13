64ef7ace91263c98ffe819b4bfa50aa87f546c65
Otagout <oussama.tagout2015@gmail.com>
2026-08-13 17:40:18 +0100

    refactor: replace backend with new Spring Boot project

---
+++ BackEnd/weekly-planner/src/test/java/com/example/weekly_planner/WeeklyPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weekly_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeeklyPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
